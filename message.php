75f9db9fea2baab54673cde43b2ae92405062ef4
Cesar Trejo <ctrejo@g.clemson.edu>
2016-04-15 17:02:05 -0400

    Fri Apr 15 17:02:05 EDT 2016

---
+++ message.php
@@ -0,0 +1,95 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+  <title>MeTube Home</title>
+  <meta name="description" content="website description" />
+  <meta name="keywords" content="website keywords, website keywords" />
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+  <link rel="stylesheet" type="text/css" href="css/style2.css" title="style" />
+  <style>
+		 body {
+			background-color: black;
+		}
+  </style>
+</head>
+
+<body>
+  <?php 
+    session_start();
+    if(empty($_SESSION['username'])){
+      Print '<script>alert("User not found");</script>'; //Prompts the user
+      Print '<script>window.location.assign("index.php");</script>';
+    }
+  ?>
+  <div id="main">
+    <div id="header">
+      <div id="logo">
+        <div id="logo_text">
+    			<div class="right">				
+    				<img src="images/metube_logo.png" alt="Clemson" style="width:191px;height:60px;">
+             <h2> Welcome
+              <?php
+                echo $_SESSION['username'];
+              ?>
+            </h2>
+    			</div>
+        </div>
+      </div>
+
+      <div id="menubar">
+        <ul id="menu">
+          <!-- put class="selected" in the li tag for the selected page - to highlight which page you're on -->
+          <li class="selected"><a href="home.php">Home</a></li>
+          <li><a href="browse.php">Browse</a></li>
+          <li><a href="upload.php">Upload</a></li>
+          <li><a href="message.php">Messages</a></li>
+          <li><div class="right"><form action="search.php" method="get"><input type="text" name="search_query" placeholder="search" required><input value="Search" name="submit_search" type="submit" /></form></div></li>
+          <li><a href="logout.php">Logout</a></li>
+        </ul>
+      </div>
+    </div>
+    <div id="site_content">
+      <div class="sidebar">
+        <!-- insert your sidebar items here -->
+
+        <h3>Suggestions</h3>
+        <ul>
+          <li>coming soon</li>
+
+        </ul>
+        <!--<h3>Search</h3>
+        <form method="post" action="#" id="search_form">
+          <p>
+            <input class="search" type="text" name="search_field" value="Enter keywords....." />
+            <input name="search" type="image" style="border: 0; margin: 0 0 -9px 5px;" src="style/search.png" alt="Search" title="Search" />
+          </p>
+        </form>-->
+      </div>
+      <div id="content">
+        <!-- insert the page content here -->
+		<h1>Select a file to upload</h1>
+    <form method="post" action="media_upload_process.php" enctype="multipart/form-data" >
+
+      Enter Title: <input type="text" name = "title"></input>
+   
+      <p style="margin:0; padding:0">
+      <input type="hidden" name="MAX_FILE_SIZE" value="10485760" />
+       Add a Media: <label style="color:#663399"><em> (Each file limit 10M)</em></label><br/>
+       <input  name="file" type="file" size="50" />
+      
+      <input value="Upload" name="submit" type="submit" />
+      </p>
+     
+                  
+    </form>
+
+		
+      </div>
+    </div>
+    <div id="footer">
+      Copyright &copy; Soham Parekh 2016
+    </div>
+  </div>
+</body>
+</html>
