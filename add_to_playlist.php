3ce58b889290e8ced80878e1f1e880f9174cde23
Soham Ashok Parekh <sohamap@imp9.cs.clemson.edu>
2016-04-17 14:44:27 -0400

    Add media to playlist

--- add_to_playlist.php
+++ add_to_playlist.php
@@ -1,66 +1,18 @@
-<!DOCTYPE HTML>
-<html>
-
-<head>
-  <title>Request</title>
-  <meta name="description" content="website description" />
-  <meta name="keywords" content="website keywords, website keywords" />
-  <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-  <link rel="stylesheet" type="text/css" href="css/style2.css" title="style" />
-  <style>
-		 body {
-			background-color: black;
-		}
-  </style>
-</head>
-
-<body>
-  <?php 
-    session_start();
+<?php
+	session_start();
     include_once "function.php";
     if(empty($_SESSION['username'])){
       Print '<script>alert("User not found");</script>'; //Prompts the user
       Print '<script>window.location.assign("index.php");</script>';
-
     }
-  ?>
-  <div id="main">
-    <div id="header">
-      <div id="logo">
-        <div id="logo_text">
-    			<div class="right">				
-    				<img src="images/metube_logo.png" alt="Clemson" style="width:191px;height:60px;">
-             <h2> Welcome
-              <?php
-                echo $_SESSION['username'];
-              ?>
-            </h2>
-    			</div>
-        </div>
-      </div>
-
-      <div id="menubar">
-        <ul id="menu">
-          <!-- put class="selected" in the li tag for the selected page - to highlight which page you're on -->
-          <li class="selected"><a href="home.php">Home</a></li>
-          <li><a href="browse.php">Browse</a></li>
-          <li><a href="upload.php">Upload</a></li>
-          <li><a href="message.php">Messages</a></li>
-          <li><div class="right"><form action="search.php" method="get" ><input type="text" name="search_query" placeholder="search" required><input value="Search" name="submit_search" type="submit" /></form></div></li>
-          <li><a href="logout.php">Logout</a></li>
-        </ul>
-      </div>
-    </div>
-    <div id="site_content">
-    <h1> Added to friends </h1>
-      <?php 
-        add_friend($_SESSION['username'],$_SESSION['uname']);
-        header("Location: home.php");
-      ?>
-    </div>
-    <div id="footer">
-      Copyright &copy; Soham Parekh 2016
-    </div>
-  </div>
-</body>
-</html>
+	if(isset($_POST['submit_add_to_playlist']))	{
+		if(add_to_playlist($_POST['playlistid'], $_GET['mediaid']) ){
+		 Print '<script>alert("Added to playlist!");</script>'; //Prompts the user
+	     Print '<script>window.location.assign("playlist.php");</script>';
+	    }
+	    else{
+		 Print '<script>alert("Could not add to playlist");</script>'; //Prompts the user
+	     Print '<script>window.location.assign("playlist.php");</script>';
+	    }
+	}
+?>
